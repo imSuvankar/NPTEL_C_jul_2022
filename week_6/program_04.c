070f20278a50c9a8cd06d5b85b3b828add93e28d
Suvankar Das <suvankar_das@outlook.com>
2022-09-02 21:45:09 +0530

    Create program_04.c

---
+++ week_6/program_04.c
@@ -0,0 +1,35 @@
+/* Write a C Program to delete duplicate elements from an array of integers. */
+
+
+
+#include<stdio.h>
+int main() 
+{
+    int array[50], i, size;
+    scanf("%d", &size); /*Accepts the size of array from test case data */
+
+    for (i = 0; i< size; i++)
+        scanf("%d", &array[i]); /* Read the array elements from the test case data */
+
+
+
+    for(i=0; i<size; i++){
+        for(int j = i+1; j < size; j++){
+
+            if(array[i] == array[j]){
+
+                for(int k=j; k<size; k++) 
+                    array[k] = array[k+1];
+
+                j--;
+                size--;
+            }
+        }
+    }
+
+
+
+    for (i = 0; i< size; i++) {
+        printf("%d\n", array[i]);
+    }
+}
