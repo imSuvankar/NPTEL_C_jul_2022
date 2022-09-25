91ae2bb96c15e22f51f13b0b9e05544e837bfbf9
Suvankar Das <suvankar_das@outlook.com>
2022-09-25 20:16:01 +0530

    Create program_01.c

---
+++ week_8/program_01.c
@@ -0,0 +1,21 @@
+// Write a C Program to find HCF of 4 given numbers using recursive function
+
+
+#include<stdio.h>
+int HCF(int, int); //You have to write this function which calculates the HCF. 
+	 
+int main()
+{
+   int a, b, c, d, result;
+   scanf("%d %d %d %d", &a, &b, &c, &d); /* Takes 4 number as input from the test data */
+   result = HCF(HCF(a, b), HCF(c,d));
+   printf("The HCF is %d", result);
+}
+
+//Complete the rest of the program to calculate HCF
+
+
+int HCF(int a, int b) {
+	while(a != b) (a > b) ? (a -= b) : (b -= a);
+  	return a;
+} 
