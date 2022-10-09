b7767b30794450e3394770b724ebba73eb8a2d9b
Suvankar Das <suvankar_das@outlook.com>
2022-10-09 23:32:20 +0530

    Update program_02.c

--- week_11/program_02.c
+++ week_11/program_02.c
@@ -1,5 +1,5 @@
 /*
-Write a C program t find ∫<sup>b</sup><sub>a</sub> x² dx using Trapezoidal rule with 10 segments between a and b.
+Write a C program t find b∫a x² dx using Trapezoidal rule with 10 segments between a and b.
 The values of a and b will be taken from test cases.
 /*
 
